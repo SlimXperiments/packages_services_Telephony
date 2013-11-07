2e4aec924b75f59cb50c54039bd0b40fbc3e4e41
Danny Baumann <dannybaumann@web.de>
2013-11-07 16:39:37 +0100

    Add back supplementary service notification handling.
    Change-Id: I028716c386d51c1649542b6c44395159dd90bcc5
    
    Conflicts:
    	src/com/android/phone/CallModeler.java

--- common/src/com/android/services/telephony/common/Call.java
+++ common/src/com/android/services/telephony/common/Call.java
@@ -145,6 +145,12 @@ public final class Call implements Parcelable {
     // Gateway service package name
     private String mGatewayPackage;
 
+    // Whether the call was forwarded from another party (GSM only)
+    private boolean mForwarded;
+
+    // Whether the call is held remotely
+    private boolean mHeldRemotely;
+
     public Call(int callId) {
         mCallId = callId;
         mIdentification = new CallIdentification(mCallId);
@@ -160,6 +166,8 @@ public final class Call implements Parcelable {
         mChildCallIds = new TreeSet<Integer>(call.mChildCallIds);
         mGatewayNumber = call.mGatewayNumber;
         mGatewayPackage = call.mGatewayPackage;
+        mForwarded = call.mForwarded;
+        mHeldRemotely = call.mHeldRemotely;
     }
 
     public int getCallId() {
@@ -248,6 +256,14 @@ public final class Call implements Parcelable {
         return mConnectTime;
     }
 
+    public boolean isForwarded() {
+        return mForwarded;
+    }
+
+    public boolean isHeldRemotely() {
+        return mHeldRemotely;
+    }
+
     public void removeChildId(int id) {
         mChildCallIds.remove(id);
     }
@@ -284,6 +300,14 @@ public final class Call implements Parcelable {
         mGatewayPackage = packageName;
     }
 
+    public void setForwarded(boolean forwarded) {
+        mForwarded = forwarded;
+    }
+
+    public void setHeldRemotely(boolean heldRemotely) {
+        mHeldRemotely = heldRemotely;
+    }
+
     /**
      * Parcelable implementation
      */
@@ -299,6 +323,8 @@ public final class Call implements Parcelable {
         dest.writeString(getGatewayNumber());
         dest.writeString(getGatewayPackage());
         dest.writeParcelable(mIdentification, 0);
+        dest.writeInt(mForwarded ? 1 : 0);
+        dest.writeInt(mHeldRemotely ? 1 : 0);
     }
 
     /**
@@ -314,6 +340,8 @@ public final class Call implements Parcelable {
         mGatewayNumber = in.readString();
         mGatewayPackage = in.readString();
         mIdentification = in.readParcelable(CallIdentification.class.getClassLoader());
+        mForwarded = in.readInt() != 0;
+        mHeldRemotely = in.readInt() != 0;
     }
 
     @Override
@@ -350,6 +378,8 @@ public final class Call implements Parcelable {
                 .add("mGatewayNumber", MoreStrings.toSafeString(mGatewayNumber))
                 .add("mGatewayPackage", mGatewayPackage)
                 .add("mIdentification", mIdentification)
+                .add("mForwarded", mForwarded)
+                .add("mHeldRemotely", mHeldRemotely)
                 .toString();
     }
 }
